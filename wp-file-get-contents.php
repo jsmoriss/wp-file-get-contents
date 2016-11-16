78a51d33e89e57eee7ccac927fb719cf0979bc72
JS Morisset <jsm@surniaulula.com>
2016-11-16 11:52:17 -0800

    v1.4.0-1

--- wp-file-get-contents.php
+++ wp-file-get-contents.php
@@ -10,7 +10,7 @@
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: A WordPress shortcode for PHP's file_get_contents()
- * Requires At Least: 3.5
+ * Requires At Least: 3.7
  * Tested Up To: 4.6.1
  * Version: 1.4.0-1
  * 
