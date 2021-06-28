33061e76ae29ba505f7280500bd35b7ef1d51bfd
JS Morisset <jsm@surniaulula.com>
2021-06-28 06:38:02 -0700

    v2.3.0

--- wp-file-get-contents.php
+++ wp-file-get-contents.php
@@ -8,7 +8,7 @@
  * Author: JS Morisset
  * Author URI: https://surniaulula.com/
  * License: GPLv3
- * License URI: http://www.gnu.org/licenses/gpl.txt
+ * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: A WordPress shortcode for PHP's file_get_contents()
  * Requires PHP: 7.0
  * Requires At Least: 4.5
