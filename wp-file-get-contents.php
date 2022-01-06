6096bfbfd540a50495a7b96df09184291b8db7f8
JS Morisset <jsm@surniaulula.com>
2022-01-06 15:29:33 -0800

    v2.4.0

--- wp-file-get-contents.php
+++ wp-file-get-contents.php
@@ -12,7 +12,7 @@
  * Description: A WordPress shortcode for PHP's file_get_contents()
  * Requires PHP: 7.2
  * Requires At Least: 5.2
- * Tested Up To: 5.8.2
+ * Tested Up To: 5.8.3
  * Version: 2.4.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
@@ -22,7 +22,7 @@
  *      {bugfix}        Backwards-compatible bug fixes or small improvements.
  *      {stage}.{level} Pre-production release: dev < a (alpha) < b (beta) < rc (release candidate).
  *
- * Copyright 2012-2021 Jean-Sebastien Morisset (https://surniaulula.com/)
+ * Copyright 2012-2022 Jean-Sebastien Morisset (https://surniaulula.com/)
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
