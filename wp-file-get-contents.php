5d52e6d9a23905957d2b82afdc055718453a200e
JS Morisset <jsm@surniaulula.com>
2023-05-13 16:04:37 -0700

    v2.6.0

--- wp-file-get-contents.php
+++ wp-file-get-contents.php
@@ -207,7 +207,7 @@ if ( ! class_exists( 'WPFGC' ) ) {
 			 */
 			if ( $do_code ) {
 
-				$content = '<code>' . "\n" . $content . '</code>' . "\n";
+				$content = '<code>' . $content . '</code>';
 			}
 
 			/*
