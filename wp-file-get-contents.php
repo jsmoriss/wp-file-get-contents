56e735876140e134a48d2063be1ed723be19bdbe
JS Morisset <jsm@surniaulula.com>
2023-05-17 11:20:39 -0700

    v2.7.0

--- wp-file-get-contents.php
+++ wp-file-get-contents.php
@@ -12,7 +12,7 @@
  * Description: A WordPress shortcode for PHP's file_get_contents()
  * Requires PHP: 7.2.5
  * Requires At Least: 5.5
- * Tested Up To: 6.2.0
+ * Tested Up To: 6.2.1
  * Version: 2.7.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
