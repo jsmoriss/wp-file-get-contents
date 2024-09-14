a545e205d7f8978963587a767e014600a249b05d
jsmoriss <jsm@surniaulula.com>
2024-09-14 22:35:48 +0000

    v2.7.1

--- wp-file-get-contents.php
+++ wp-file-get-contents.php
@@ -10,8 +10,8 @@
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: A WordPress shortcode for PHP's file_get_contents()
- * Requires PHP: 7.2.34
- * Requires At Least: 5.8
+ * Requires PHP: 7.4.33
+ * Requires At Least: 5.9
  * Tested Up To: 6.6.2
  * Version: 2.7.1
  *
